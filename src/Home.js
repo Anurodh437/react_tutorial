514c21171592e5f42c982c7922175c962ba64a0e
Anurodh437 <anurodh.dubey_cs19@ga.ac.in>
2021-08-31 18:55:38 +0530

    modified

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+  //let name = "mario";
+  const [name, setName] = useState('mario');
+  const [age,setAge] = useState(25)
 
-  const handleClick = (e) => {
-    console.log("Hello, Ninjas!",e);
-  }
-
-  const handleClickAgain = (name,e) =>{
-    console.log("Hello " + name, e.target);
+  const handleClick = () => {
+    setName('Anurodh');
+    setAge(56);
   }
 
   return (
     <div className="home">
       <h2>HomePage</h2>
+      <p>{name} is {age} years old</p>
       <button onClick={handleClick} >Click me</button>
-      <button onClick={(e) => handleClickAgain("Anurodh",e)} >Click Me Again</button>
     </div>
   );
 }
