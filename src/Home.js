2af304c970a946ec4ab1d78ec5687b5bbb3e6285
Anurodh437 <anurodh.dubey_cs19@ga.ac.in>
2021-09-01 16:01:50 +0530

    made some changes

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
+import BlogList from "./blogList";
 
 const Home = () => {
-  //let name = "mario";
-  const [name, setName] = useState('mario');
-  const [age,setAge] = useState(25)
-
-  const handleClick = () => {
-    setName('Anurodh');
-    setAge(56);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
   return (
     <div className="home">
-      <h2>HomePage</h2>
-      <p>{name} is {age} years old</p>
-      <button onClick={handleClick} >Click me</button>
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs" />
+
     </div>
   );
 }
