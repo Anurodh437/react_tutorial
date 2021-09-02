cfbca7ef4793bf610713eafca43366400b37d732
Anurodh437 <anurodh.dubey_cs19@ga.ac.in>
2021-09-02 18:00:34 +0530

    added new files with some style changes

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,8 @@
-import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const {data:  blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="home">
